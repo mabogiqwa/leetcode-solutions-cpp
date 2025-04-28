96b753ccead327eaecec4562ece11c655bd06784
Mabo Giqwa <mabogiqwa1@gmail.com>
2025-04-28 15:58:34 +0200

    Adding deep copy function

--- merge_link_list.cpp
+++ merge_link_list.cpp
@@ -22,17 +22,39 @@ void add_node(NodePtr &head, int val);
 
 void print(NodePtr &head);
 
-void merge_lists(NodePtr &h1, NodePtr &h2);
-//Postcondition: Merges two linked lists
+void deallocate_list(NodePtr &head);
+
+//Version 1
+Node* merge_lists(NodePtr h1, NodePtr h2);
+//Precondition: Pass the first pointer of the first list and the head of the second list
+//Postcondition: Returns a merged list of the two linked lists
 
 int main()
 {
     NodePtr first = initialize_list(3);
     NodePtr head = first;
 
-    add_node(first, 6);
+    add_node(first, 4);
+    add_node(first, 7);
+    add_node(first, 2);
+    add_node(first, 1);
+    add_node(first, 10);
 
-    print(head);
+    NodePtr first2 = initialize_list(5);
+    NodePtr head2 = first2;
+
+    add_node(first2, 12);
+    add_node(first2, 18);
+    add_node(first2, 19);
+    add_node(first2, 22);
+    add_node(first2, 14);
+
+    NodePtr mergedHead = merge_lists(head, head2);
+
+    deallocate_list(head);
+    deallocate_list(head2);
+
+    print(mergedHead);
 
     return 0;
 }
@@ -44,6 +66,7 @@ void print(NodePtr &head)
     {
         std::cout << tempPtr->value << " ";
     }
+    std::cout << std::endl;
 }
 
 void add_node(NodePtr &last, int val)
@@ -54,4 +77,50 @@ void add_node(NodePtr &last, int val)
     last = tempPtr;
     tempPtr->link = nullptr;
     size++;
+}
+
+void deallocate_list(NodePtr &head)
+{
+    NodePtr tempPtr = head;
+    NodePtr currentNode;
+    while (tempPtr->link != nullptr)
+    {
+        currentNode = tempPtr;
+        tempPtr = tempPtr->link;
+        delete currentNode;
+    }
+    head = nullptr;
+}
+
+Node* merge_lists(NodePtr h1, NodePtr h2)
+{
+    NodePtr newHead = h1;
+    NodePtr tempPtr, lastNode;
+
+    //Deep copy
+    for (tempPtr = newHead; tempPtr != nullptr; tempPtr = tempPtr->link)
+    {
+
+    }
+
+    //Iterate through the first linked list
+    for (tempPtr = newHead; tempPtr != nullptr; tempPtr = tempPtr->link)
+    {
+        if (tempPtr->link == nullptr)
+            lastNode = tempPtr;
+    }
+
+    lastNode->link = h2; //merge linked lists
+
+    return newHead;
+}
+
+Node* deep_copy(NodePtr &head)
+{
+    NodePtr tempPtr;
+    for (tempPtr = newHead; tempPtr != nullptr; tempPtr = tempPtr->link)
+    {
+
+    }
+
 }
