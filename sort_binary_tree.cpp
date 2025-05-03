d4bf40b42ee7198cd8f6c19fa558212a58472249
Mabo Giqwa <mabogiqwa1@gmail.com>
2025-05-03 16:40:15 +0200

    Initial binary search tree code

---
+++ sort_binary_tree.cpp
@@ -0,0 +1,19 @@
+//Sorts a unordered binary tree to a binary search tree (BST)
+#include <iostream>
+
+struct Node
+{
+    int value;
+    Node *rLink;
+    Node *lLink;
+};
+typedef Node* NodePtr;
+
+void add_node(NodePtr &head, int value);
+//Postcondition: Adds nodes to the left or right link
+
+int main()
+{
+
+    return 0;
+}
