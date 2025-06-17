7d26bc729ac68717fca777ad528b0b5fa9013f5f
Mabo Giqwa <mabogiqwa1@gmail.com>
2025-06-17 05:42:53 +0200

    New commit

--- add-two-numbers.cpp
+++ add-two-numbers.cpp
@@ -16,6 +16,10 @@ class LinkedList
 public:
     NodePtr head;
 
+    LinkedList() {
+        head = nullptr;
+    }
+
     LinkedList(int val) {
         head = new Node(val);
     }
@@ -52,18 +56,73 @@ public:
     }
 };
 
+long long convertToDigit(LinkedList list)
+{
+    NodePtr iter;
+    int iteration = 0;
+    long long digit = 0;
+
+    for (iter = list.head; iter != nullptr; iter = iter->link)
+    {
+        digit += (iter->value * (pow(10, iteration)));
+        iteration++;
+    }
+
+    return digit;
+}
+
+Node* construct_list(long long num)
+{
+    std::string numString = std::to_string(num);
+    int size = numString.size();
+
+    int lastNumber = std::stoi(std::string(1, numString[size-1]));
+    LinkedList newList(lastNumber);
+
+    for (int i = numString.size() - 2; i >= 0; i--)
+    {
+        int currentNumber = std::stoi(std::string(1, numString[i]));
+        newList.add_node(currentNumber);
+    }
+
+    return newList.head;
+}
+
+LinkedList addTwoNumbers(LinkedList l1, LinkedList l2)
+{
+    LinkedList l3;
+    long long total = convertToDigit(l1) + convertToDigit(l2);
+
+    l3.head = construct_list(total);
+
+    return l3;
+}
+
 int main()
 {
-    LinkedList l1(2);
-    l1.add_node(4);
-    l1.add_node(3);
+    LinkedList l1(9);
+    l1.add_node(9);
+    l1.add_node(9);
+    l1.add_node(9);
+    l1.add_node(9);
+    l1.add_node(9);
+    l1.add_node(9);
 
-    LinkedList l2(5);
-    l2.add_node(6);
-    l2.add_node(4);
+    LinkedList l2(9);
+    l2.add_node(9);
+    l2.add_node(9);
+    l2.add_node(9);
+
+    long long total = convertToDigit(l1) + convertToDigit(l2);
+
+    LinkedList l3;
+    l3 = addTwoNumbers(l1, l2);
+
+    l3.print();
 
     l1.deallocate();
     l2.deallocate();
+    l3.deallocate();
 
     return 0;
 }
