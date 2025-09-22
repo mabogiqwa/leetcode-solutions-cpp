7ab64b1077c59b2b0bf622255b18b51670f22cd3
mabogiqwa <mabogiqwa1@gmail.com>
2025-09-22 14:37:14 +0200

    new commit

--- longest-common-prefix.cpp
+++ longest-common-prefix.cpp
@@ -9,20 +9,22 @@ std::string longestCommonPrefix(std::vector<std::string> strs)
     std::string firstWord = "";
     int iterations = 0;
     int length, lengthOfPrefix = 0;
-    std::string nextString;
+    std::string nextStringLength;
     bool longestPrefixExists = false;
 
     firstWord = strs[0];
     length = strs[0].length();
+    nextStringLength = strs[iterations+1].length();
     while (iterations < strs.size())
     {
-        nextString = strs[iterations+1].length();
         //compares the first and second words and captures the longest prefix
         if (index == 0) {
-            for (int i = 0; i < strs[iterations+1].length(); i++) {
-                if (firstWord[0] != nextString[0]) {
+            for (int i = 0; i < nextStringLength; i++) { //also fix this
+                if (firstWord[0] != nextString[0]) { //checks if the first character match or not
                     return "";
+                    i++;
                 }
+                break; //end the for loop
                 if (firstWord[i] == nextString[i]) {
                     longestPrefixExists = true;
                     longestCPrfx += (firstWord[i]);
