98377218197840841347227ea967c43257f4b275
Mabo Giqwa <mabogiqwa1@gmail.com>
2025-09-27 20:47:54 +0200

    new commit

--- largest-triangle-area.cpp
+++ largest-triangle-area.cpp
@@ -1,82 +1,39 @@
 #include <iostream>
+#include <cmath>
 #include <vector>
-#include <algorithm>
-#include <limits>
 
-void print(std::vector<double> areas) {
-    for (int i = 0; i < areas.size(); i++) {
-        std::cout << areas[i] << " ";
-    }
-    std::cout << std::endl;
-}
-
-int largest(int i, int j, int k) {
-    double largest = -std::numeric_limits<double>::infinity();
-
-    if (i > largest) {
-        largest = i;
-        if (j > largest) {
-            largest = j;
-            if (k > largest) {
-                largest = k;
-            }
-        }
-    }
-
-    return int(largest);
-}
-
-int smallest(int i, int j, int k) {
-    double smallest = std::numeric_limits<double>::infinity();
-
-    if (i < smallest) {
-        smallest = i;
-        if (j < smallest) {
-            smallest = j;
-            if (k < smallest) {
-                smallest = k;
-            }
-        }
-    }
-
-    return int(smallest);
-}
-
-//the triangle doesn't have to begin at the origin
-double largestTriangleArea(std::vector<std::vector<int>> points) {
-    std::vector<double> areas;
-    double area, breadth, height;
-
-    if (points.size() >= 3) {
-        int k, l;
-
-        for (int i = 0; i < points.size() - 2; i++) {
-            k = i+1;
-            l = i+2;
-            //height = points[i][1] + points[k][1] + points[l][1];
-            //breadth = points[i][0] + points[k][0] + points[l][0];
-
-            area = (1.0/2.0)*breadth*height;
-            areas.push_back(area);
-        }
-    } else {
-        return -1;
-    }
-    print(areas);
-
-    auto max = std::max_element(areas.begin(), areas.end());
-    double largestArea = *max;
-
-    return largestArea;
+double triangle_area(const std::vector<int>& a,
+                     const std::vector<int>& b,
+                     const std::vector<int>& c) {
+    int x1 = a[0], y1 = a[1];
+    int x2 = b[0], y2 = b[1];
+    int x3 = c[0], y3 = c[1];
+    return std::abs(x1*(y2 - y3) + x2*(y3 - y1) + x3*(y1 - y2)) / 2.0;
 }
 
 int main() {
+    std::vector<std::vector<int>> points = {{0,0},{0,1},{1,0},{0,2},{2,0}};
+    double max_area = 0;
+    std::vector<std::vector<int>> best_triangle;
 
-  std::vector<std::vector<int>> points = {{0,0},{0,1},{1,0},{0,2},{2,0}};
+    int n = points.size();
+    for(int i=0; i<n; ++i) {
+        for(int j=i+1; j<n; ++j) {
+            for(int k=j+1; k<n; ++k) {
+                double area = triangle_area(points[i], points[j], points[k]);
+                if(area > max_area) {
+                    max_area = area;
+                    best_triangle = {points[i], points[j], points[k]};
+                }
+            }
+        }
+    }
 
-  //std::cout << "Output: " << largestTriangleArea(points);
-  std::cout << largest(10,19,7) << std::endl;
-  std::cout << smallest(14,5,19);
+    std::cout << "Largest area: " << max_area << std::endl;
+    std::cout << "Triangle points: ";
+    for(auto &p : best_triangle)
+        std::cout << "(" << p[0] << "," << p[1] << ") ";
+    std::cout << std::endl;
 
-  return 0;
+    return 0;
 }
