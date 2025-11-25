a0eb328070491b4a5f8e7b54287c97b31300453b
mabogiqwa <mabogiqwa1@gmail.com>
2025-11-25 23:16:01 +0200

    first commit

---
+++ longest-substring-without-repeating-characters.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+
+void lengthOfLongestSubstring(std::string s) {
+
+   std::string stringWithoutRep = "";
+   for (int i = 0; i < s.size()-1; i++) {
+       if (s[i] != s[i+1]) {
+           stringWithoutRep += s[i];
+       }
+   }
+   std::cout << stringWithoutRep;
+}
+
+int main()
+{
+    std::string s = "abcabcbb";
+
+    lengthOfLongestSubstring(s);
+
+    return 0;
+}
