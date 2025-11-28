d47f1ebd22fd35c54a21535fe74b3fd30ca8c4a4
mabogiqwa <mabogiqwa1@gmail.com>
2025-11-28 10:56:33 +0200

    Initial commit

--- container-with-most-water.cpp
+++ container-with-most-water.cpp
@@ -1,10 +1,30 @@
 #include <iostream>
+#include <vector>
+#include <limits>
 
-int maxArea(std::vector<int>& height) {
+int findLargest(std::vector<int>& height) {
+    int alpha = std::numeric_limits<int>::min();
 
+    for (int i = 0; i < height.size(); i++) {
+        if (height[i] > alpha)
+            alpha = height[i];
+    }
+
+    return alpha;
 }
 
-int main() {
+int maxArea(std::vector<int>& height)
+{
+    for (int i = 1; i < height.size(); i++) {
+
+    }
+}
+
+int main()
+{
+    std::vector<int> h = {1,8,6,2,5,4,8,3,7};
+
+    std::cout << findLargest(h);
 
     return 0;
 }
