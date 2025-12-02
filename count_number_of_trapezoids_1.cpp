09fce2adf245a5490f32f8bfc6f3bb63a94b8a9f
mabogiqwa <mabogiqwa1@gmail.com>
2025-12-02 22:28:41 +0200

    Logic error in factorial function

--- count_number_of_trapezoids_1.cpp
+++ count_number_of_trapezoids_1.cpp
@@ -1,10 +1,37 @@
 #include <iostream>
 #include <vector>
 
-int countTrapezoids(std::vector<std::vector<int>>& points) {
-    return 0;
+long long factorial(int n) {
+    long long result = 1;
+
+    if (n < 0) { return 0; }
+    if (n == 0 || n == 1) {
+        return 1;
+    }
+
+    for (int i = 2; i <= n; i++) {
+        result *= i;
+    }
+
+    return result;
 }
 
+void print(std::vector<int>& sums) {
+    for (int i = 0; i < sums.size(); i++) {
+        std::cout << sums[i] << " ";
+    }
+    std::cout << std::endl;
+}
+
+long long combination(int n, int r) {
+    long long result;
+
+    result = factorial(n)/(factorial(r)*factorial(n - r));
+
+    return result;
+}
+
+//This will be needed for computing the combinations
 int computeDistinctYCoordinates(std::vector<std::vector<int>>& points) {
     int total = 0;
     int initialYVal = points[0][1];
@@ -17,11 +44,39 @@ int computeDistinctYCoordinates(std::vector<std::vector<int>>& points) {
     return total;
 }
 
+//factorial function is buggy
+int countTrapezoids(std::vector<std::vector<int>>& points) {
+    int initialYVal = points[0][1];
+    std::vector<int> numOfPoints;
+    int sum = 0, numOfYPoints;
+    long long multiplication = 1, combination;
+
+    for (int i = 0; i < points.size();i++) {
+        if (points[i][1] == initialYVal) {
+            sum += 1;
+        } else {
+            initialYVal = points[i][1];
+            numOfPoints.push_back(sum);
+            sum = 1;
+        }
+    }
+    numOfPoints.push_back(sum);
+
+    numOfYPoints = computeDistinctYCoordinates(points);
+    for (int i = 0; i < numOfPoints.size(); i++) {
+        combination = (numOfPoints[i], numOfYPoints);
+        std::cout << "Combination: " << combination << std::endl;
+        multiplication *= combination;
+    }
+
+    return multiplication;
+}
+
 int main()
 {
-    std::vector<std::vector<int>> points = {{0,0},{1,0},{0,1},{2,1}};
+    std::vector<std::vector<int>> points = {{1,0},{2,0},{3,0},{2,2},{3,2}};
 
-    std::cout << computeDistinctYCoordinates(points) << std::endl;
+    std::cout << countTrapezoids(points);
 
     return 0;
 }
